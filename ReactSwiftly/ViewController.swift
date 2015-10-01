3e609ca10738d179a7f562404f8fefb46e978721
Sam Youtsey <sam.youtsey@gmail.com>
2015-09-30 23:38:33 -0700

    Initial commit

--- ReactSwiftly/ViewController.swift
+++ ReactSwiftly/ViewController.swift
@@ -1,25 +1,57 @@
 //
 //  ViewController.swift
-//  ReactSwiftly
+//  NSInventory
 //
 //  Created by Sam Youtsey on 9/30/15.
 //  Copyright © 2015 samswiches. All rights reserved.
 //
 
 import UIKit
+import ReactiveCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let localURL = "http://localhost:8001/hello/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let searchStrings = self.textField.rac_textSignal().toSignalProducer().map { text in text }
+        let searchResults = searchStrings
+            .flatMap(.Latest) { query in
+                let name = query as! String
+                let URLRequest = NSURLRequest(URL: NSURL(string: self.localURL + name)!)
+                
+                return NSURLSession.sharedSession()
+                    .rac_dataWithRequest(URLRequest)
+                    .retry(2)
+                    .flatMapError { error in
+                        print("Network error occurred: \(error)")
+                        return SignalProducer.empty
+                }
+            }
+            .map({ (data: NSData, response: NSURLResponse) -> String in
+                return self.parseJSONResultsFromData(data)
+            })
+            .observeOn(UIScheduler())
+        
+        searchResults.start({ results in
+            self.nameLabel.text = "Hello " + results.value! + "!"
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func parseJSONResultsFromData(data: NSData) -> String {
+        do {
+            let jsonData = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as! NSDictionary
+            let name = jsonData.objectForKey("name") as! String
+            return name
+        } catch let error as NSError {
+            print("ERROR: \(error)")
+            return ""
+        }
     }
-
-
 }
 
