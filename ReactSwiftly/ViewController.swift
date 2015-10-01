e5ced9c134883a89aaa092f1eace8eb94ef89235
Sam Youtsey <sam.youtsey@gmail.com>
2015-09-30 23:24:00 -0700

    Initial Commit

---
+++ ReactSwiftly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactSwiftly
+//
+//  Created by Sam Youtsey on 9/30/15.
+//  Copyright © 2015 samswiches. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
